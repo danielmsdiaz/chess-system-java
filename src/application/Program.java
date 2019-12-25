24ebbba0aead78a6524028c2d89a3215379b7f61
Daniel Diaz <danielmsdiaz@hotmail.com>
2019-12-25 15:08:05 -0300

    implementando tabuleiro e peça

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position p1 = new Position(2, 3);
-		System.out.println(p1);
+		Board board = new Board(8, 8);
 	}
 
 }
