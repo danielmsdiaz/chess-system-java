8c7dc8e2d6951c43bac15799bce57cc2a3f063a5
Daniel Diaz <danielmsdiaz@hotmail.com>
2019-12-28 17:14:38 -0300

    movimentos possiveis para uma peca

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -12,6 +12,24 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();       //retorna uma matriz de movimentos possiveis
+	
+	public boolean possibleMove(Position position) {     // retorna verdadeiro ou falso se for possivel mover para essa posicao
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {           //checa se há algum movimento possivel, ou a peca ta travada
+		boolean[][] mat = possibleMoves();
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j< mat.length; j++) {
+				if(mat[i][j] == true) {                 
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 	
